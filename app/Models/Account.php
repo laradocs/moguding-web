04f7c58e1867adc83aeaffcc43744b5e24f50ff7
laradocs <75692074@qq.com>
2022-03-07 00:06:36 +0800

    更新模型

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,10 +2,44 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
 class Account extends Model
 {
-    use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'accounts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'user_id',
+        'device',
+        'phone',
+        'password',
+        'status',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+        'device' => 'string',
+        'phone' => 'integer',
+        'password' => 'string',
+        'status' => 'boolean',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 }
