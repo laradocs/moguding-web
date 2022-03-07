5f5e8d6494fe680c2b1d398d4a2d7c2423790db0
laradocs <75692074@qq.com>
2022-03-07 15:12:23 +0800

    添加账户按钮

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -4,33 +4,41 @@
 @section ( 'content' )
     @include ( 'layouts._header' )
 
-    <table id="table" data-toggle="table">
-        <thead>
-        <tr>
-            <th data-sortable="true" data-field="id">ID</th>
-            <th data-field="device">设备</th>
-            <th data-field="phone">手机号码</th>
-            <th data-field="status">状态</th>
-            <th>操作</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ( $accounts as $account )
+    <div class="card">
+        <div class="card-header">
+            <a class="btn btn-sm btn-primary" href="" title="">
+                <i class="fa-solid fa-plus"></i>
+                <span>添加账户</span>
+            </a>
+        </div>
+        <table id="table" data-toggle="table">
+            <thead>
             <tr>
-                <td>{{ $account->id }}</td>
-                <td>{{ $account->device }}</td>
-                <td>{{ $account->phone }}</td>
-                <td>{!! $account->status ? '<span class="badge badge-success">正常</span>' : '<span class="badge badge-danger">异常</span>' !!}</td>
-                <td>
-                    <a class="btn btn-sm btn-info" href="" title="">
-                        <i class="fa-solid fa-sm fa-pen-to-square"></i>
-                    </a>
-                    <a class="btn btn-sm btn-danger" href="javascript:void(0);" title="">
-                        <i class="fa-solid fa-sm fa-trash-can"></i>
-                    </a>
-                </td>
+                <th data-sortable="true" data-field="id">ID</th>
+                <th data-field="device">设备</th>
+                <th data-field="phone">手机号码</th>
+                <th data-field="status">状态</th>
+                <th>操作</th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach ( $accounts as $account )
+                <tr>
+                    <td>{{ $account->id }}</td>
+                    <td>{{ $account->device }}</td>
+                    <td>{{ $account->phone }}</td>
+                    <td>{!! $account->status ? '<span class="badge badge-success">正常</span>' : '<span class="badge badge-danger">异常</span>' !!}</td>
+                    <td>
+                        <a class="btn btn-sm btn-info" href="{{ route ( 'accounts.create' ) }}" title="">
+                            <i class="fa-solid fa-sm fa-pen-to-square"></i>
+                        </a>
+                        <a class="btn btn-sm btn-danger" href="javascript:void(0);" title="">
+                            <i class="fa-solid fa-sm fa-trash-can"></i>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @stop
